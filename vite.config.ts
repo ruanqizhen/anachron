e6b486cb426e1667945b8a4d89250d8dea00815f
ruanqizhen <ruanqizhen@gmail.com>
2026-05-02 19:56:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      // Allow Cloudflare Turnstile to create Trusted Types policy in dev
+      'Content-Security-Policy': "trusted-types goog#html ymiGc5 default",
+    },
+  },
 })
